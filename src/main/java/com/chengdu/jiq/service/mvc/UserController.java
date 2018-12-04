65b332d83c1b407b80dc578f2992f2626a586cf1
jiy <yiqin.ji@dianrong.com>
2018-12-04 20:26:49 +0800

    csv

--- src/main/java/com/chengdu/jiq/service/mvc/UserController.java
+++ src/main/java/com/chengdu/jiq/service/mvc/UserController.java
@@ -2,7 +2,12 @@ package com.chengdu.jiq.service.mvc;
 
 import com.chengdu.jiq.common.annotation.ControllerAspect;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by jiyiqin on 2017/9/16.
@@ -18,4 +23,10 @@ public class UserController {
         return "user/userlist";
     }
 
+    @RequestMapping(value = "/list2")
+    public ModelAndView user2(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        ModelMap model = new ModelMap();
+        model.addAttribute("courtName", "yiqin");
+        return new ModelAndView("user/userlist", model);
+    }
 }
